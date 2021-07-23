5c98906fca7fb3bfceecedd4d009c330fecf2f33
Nicolas Molina Monroy <hi@nicobytes.com>
2021-07-23 11:30:12 -0400

    :construction: style

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  activeMenu = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMenu() {
+    this.activeMenu = !this.activeMenu;
   }
 
 }
